4f5a98a85546acc48cabe9218a6c6ea91b9ea691
pswang <pengshengwang@gmail.com>
2016-09-13 16:55:13 +0800

    up

--- auth/res/models.go
+++ auth/res/models.go
@@ -3,30 +3,30 @@ package res
 import . "github.com/eynstudio/gobreak"
 
 type AuthRes struct {
-	Id     GUID
-	Mc     string
-	Bz     string
-	Qz     int
-	Icon   string
-	Uri    string
-	Params []KeyValue
-	Opts   []Opt
+	Id   GUID
+	Mc   string
+	Bz   string
+	Qz   int
+	Icon string
+	Ns   string
+	Args []KeyValue
+	Opts []Opt
 }
 
-func (p AuthRes) GetMc() string  { return p.Mc }
-func (p AuthRes) GetUri() string { return p.Uri }
-func (p AuthRes) GetQz() int     { return p.Qz }
+func (p AuthRes) GetMc() string { return p.Mc }
+func (p AuthRes) GetNs() string { return p.Ns }
+func (p AuthRes) GetQz() int    { return p.Qz }
 
 func (p *AuthRes) ReplaceOpt(opt Entity) { Slice(&p.Opts).ReplaceEntity(opt) }
 func (p *AuthRes) DelOpt(id GUID)        { Slice(&p.Opts).RemoveEntity(id) }
 
 type Opt struct {
-	Id      GUID
-	Mc      string
-	Bz      string
-	Icon    string
-	Qz      int
-	Visible bool
-	Action  string
-	Params  []KeyValue
+	Id   GUID
+	Mc   string
+	Bz   string
+	Icon string
+	Qz   int
+	View bool
+	Act  string
+	Args []KeyValue
 }
