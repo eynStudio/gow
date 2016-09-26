ae63b499715622c12ade98210231914505cd61d7
pswang <pengshengwang@gmail.com>
2016-09-26 08:18:16 +0800

    update

--- auth/res/ctx.go
+++ auth/res/ctx.go
@@ -3,6 +3,7 @@ package res
 import (
 	"log"
 
+	"github.com/eynstudio/gobreak"
 	"github.com/eynstudio/gobreak/orm"
 	"github.com/eynstudio/gox/di"
 )
@@ -10,15 +11,22 @@ import (
 func init() {
 	log.Println("res.init")
 
-	log.Println(di.Reg(&ResCtx{Mc: "eyn"}))
+	log.Println(di.Reg(&ResCtx{}))
 }
 
 type ResCtx struct {
 	*orm.Orm `di:"*"`
-	Mc       string
 }
 
 func (p *ResCtx) All() (lst []AuthRes, err error) {
 	err = p.Orm.AllJson(&lst)
 	return
+}
+
+func (p *ResCtx) Save(m AuthRes) gobreak.IStatus {
+	err := p.Orm.SaveJson(m.Id, m)
+	if err != nil {
+		log.Println(err)
+	}
+	return gobreak.NewStatusErr(err, "保存成功", "保存失败")
 }
